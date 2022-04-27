8efac15c9c8d503e62124683556d3617b04092c1
ahmad <ahmadalisaim@outlook.com>
2022-04-27 23:47:40 +0500

    get location before sending API call

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-async function loadweather() {
+async function loadweather(lat, long) {
+  let latitude = lat;
+  let longitude = long;
   const endpoint = new URL(
-    `https://api.openweathermap.org/data/2.5/weather?lat=31.279419&lon=72.3314&units=metric&appid=6aea09e86a2248ecc6ac4a53217bd2d8`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=6aea09e86a2248ecc6ac4a53217bd2d8`
   );
   const response = await fetch(endpoint);
   console.log(response);
@@ -17,13 +19,35 @@ async function loadweather() {
   console.log(data);
 }
 
-function displayData() {
-  document.getElementById("city_name").innerHTML = city_name;
-}
+// function displayData() {}
 
 function getLocation() {
   //   console.log("get location started");
-  loadweather();
+  let options = {
+    enableHighAccuracy: true,
+    timeout: 1000 * 10, //10 seconds
+    maximumAge: 1000 * 60 * 5, //5 minutes
+  };
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, failed, options);
+  } else {
+    alert("sorry your browser doesn't support geolocation");
+  }
 }
+
+function success(position) {
+  console.log("Location obtained successfully");
+  let cords = position.coords;
+  let latitude = cords.latitude;
+  let longitude = cords.longitude;
+  console.log(position);
+
+  loadweather(latitude, longitude);
+}
+
+function failed() {
+  console.log("couldnt obtain location");
+}
+
 getLocation();
 // window.onload = getLocation();
