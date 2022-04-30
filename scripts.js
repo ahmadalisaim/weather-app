684438266dc8e88aef439caba0ea9ae793aa8e08
ahmad <ahmadalisaim@outlook.com>
2022-05-01 00:27:02 +0500

    updated layout

--- scripts.js
+++ scripts.js
@@ -1,8 +1,33 @@
+const cityht = document.getElementById("city_name");
+const countryht = document.getElementById("country_name");
+
+const add_div = (document.getElementById(
+  "current_weather_items"
+).innerHTML = `<div class="weather-item">
+<div>Humidity</div>
+<div>12%</div>
+</div>
+<div class="weather-item">
+<div>Pressure</div>
+<div>345354</div>
+</div>
+<div class="weather-item">
+<div>Wind Speed</div>
+<div>74647</div>
+</div>
+<div class="weather-item">
+<div>Possibility Of Rain in 24 hours</div>
+<div>74647</div>
+</div>
+`);
+setInterval(getTime, 1000);
+getLocation();
 async function loadweather(lat, long) {
   let latitude = lat;
   let longitude = long;
+  const app_id = "6aea09e86a2248ecc6ac4a53217bd2d8";
   const endpoint = new URL(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=6aea09e86a2248ecc6ac4a53217bd2d8`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${app_id}`
   );
   const response = await fetch(endpoint);
   console.log(response);
@@ -14,15 +39,13 @@ async function loadweather(lat, long) {
   let city_name = data.name;
   let country = data.sys.country;
   let temperature = data.main.temp;
-  document.getElementById("city_name").innerHTML = city_name;
-  document.getElementById("country_name").innerHTML = "(" + country + ")";
+  cityht.innerHTML = city_name;
+  countryht.innerHTML = "(" + country + ")";
   console.log(data);
 }
 
-// function displayData() {}
-
+//   Get Current location
 function getLocation() {
-  //   console.log("get location started");
   let options = {
     enableHighAccuracy: true,
     timeout: 1000 * 10, //10 seconds
@@ -48,6 +71,27 @@ function success(position) {
 function failed() {
   console.log("couldnt obtain location");
 }
+//Get Current Time
+function getTime() {
+  let time = new Date();
+  let hours = time.getHours() % 12;
+  let minutes = time.getMinutes();
+  //   let seconds = time.getSeconds();
 
-getLocation();
+  let session = document.getElementById("session");
+
+  if (hours === 0) {
+    hours = 12;
+  }
+  if (hours >= 12) {
+    session.innerHTML = "PM";
+  } else {
+    session.innerHTML = "AM";
+  }
+  document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
+  document.getElementById("minutes").innerHTML =
+    minutes < 10 ? "0" + minutes : minutes;
+  //   document.getElementById("seconds").innerHTML = seconds;
+}
+
 // window.onload = getLocation();
